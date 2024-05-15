65320d7c87fba3ed4978e0bf96473520a086cd9c
wslim98 <dnjstjr428@naver.com>
2024-05-15 20:09:55 +0900

    약속잡기: 중간좌표까지 구현

--- src/components/map/KakaoMap.vue
+++ src/components/map/KakaoMap.vue
@@ -4,6 +4,7 @@ export default {
   data() {
     return {
       map: null,
+      markers: [],
     };
   },
   setup() {},
@@ -31,20 +32,50 @@ export default {
     loadMap() {
       const container = document.getElementById("map");
       const options = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3,
+        center: new window.kakao.maps.LatLng(36.355332, 127.298348),
+        level: 5,
       };
       this.map = new window.kakao.maps.Map(container, options);
-      this.loadMarker();
     },
+
     // 지정한 위치에 마커 불러오기
-    loadMarker() {
-      const markerPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
+    loadMarker(position = new window.kakao.maps.LatLng(36.355332, 127.298348)) {
       const marker = new window.kakao.maps.Marker({
-        position: markerPosition,
+        position: position,
       });
 
       marker.setMap(this.map);
+    },
+
+    // 마커 추가
+    addMarker(position = new window.kakao.maps.LatLng(36.355332, 127.298348)) {
+      const marker = new window.kakao.maps.Marker({
+        position: position,
+        map: this.map,
+        draggable: true, // 마커 드래그 가능하도록 설정
+      });
+
+      // 드래그 이벤트 리스너 수정
+      window.kakao.maps.event.addListener(marker, "dragend", () => {
+        const position = marker.getPosition();
+        // 마커의 인덱스를 찾음
+        const index = this.markers.indexOf(marker);
+        this.$emit("updateMarkerPosition", {
+          index: index, // 드래그된 마커의 인덱스를 추가
+          latitude: position.getLat(),
+          longitude: position.getLng(),
+        });
+      });
+
+      this.markers.push(marker);
+    },
+    // 마커 삭제
+    removeMarker(index) {
+      // 인덱스를 매개변수로 받음
+      if (this.markers[index]) {
+        this.markers[index].setMap(null); // 해당 인덱스의 마커를 지도에서 제거
+        this.markers.splice(index, 1); // 배열에서도 해당 마커를 제거
+      }
     },
   },
 };
--- src/views/Promise.vue
+++ src/views/Promise.vue
@@ -6,6 +6,88 @@ export default {
   components: {
     KakaoMap,
   },
+  data() {
+    return {
+      coordinates: [],
+      averageLat: null,
+      averageLng: null,
+      centerMarker: null,
+    };
+  },
+  methods: {
+    addMarker() {
+      const center = this.$refs.kakaoMap.map.getCenter();
+      this.$refs.kakaoMap.addMarker(new window.kakao.maps.LatLng(center.getLat(), center.getLng()));
+
+      const newCoordinate = {
+        latitude: center.getLat(), // 중심 위도
+        longitude: center.getLng(), // 중심 경도
+      };
+      this.coordinates.push(newCoordinate);
+      console.log(this.coordinates);
+    },
+    removeMarker(index) {
+      this.$refs.kakaoMap.removeMarker(index); // 인덱스를 전달
+      this.coordinates.splice(index, 1);
+      console.log(this.coordinates);
+    },
+    handleMarkerPositionUpdate(payload) {
+      // 드래그된 마커의 인덱스와 새 위치를 받음
+      const { index, latitude, longitude } = payload;
+      if (this.coordinates.length > 0 && this.coordinates[index]) {
+        const coordinate = this.coordinates[index];
+        coordinate.latitude = latitude;
+        coordinate.longitude = longitude;
+        this.coordinates.splice(index, 1, coordinate);
+        console.log(index + 1, "드래그", this.coordinates);
+      }
+    },
+
+    centerMapOnAverage() {
+      if (this.coordinates.length === 0) {
+        return; // 좌표가 없으면 아무것도 하지 않음
+      }
+
+      // 이전에 생성된 중심 좌표의 마커가 있으면 제거
+      if (this.centerMarker) {
+        this.centerMarker.setMap(null); // 지도에서 마커 제거
+        this.centerMarker = null; // 참조 초기화
+      }
+
+      let totalLat = 0;
+      let totalLng = 0;
+
+      this.coordinates.forEach((coordinate) => {
+        totalLat += coordinate.latitude;
+        totalLng += coordinate.longitude;
+      });
+
+      this.averageLat = totalLat / this.coordinates.length;
+      this.averageLng = totalLng / this.coordinates.length;
+
+      // 지도의 중심을 평균 좌표로 이동
+      const center = new window.kakao.maps.LatLng(this.averageLat, this.averageLng);
+      this.$refs.kakaoMap.map.setCenter(center);
+
+      // 다른 이미지의 마커를 추가하기 위한 마커 이미지 정보
+      const imageSrc = "https://ifh.cc/g/6gWFKz.png"; // 마커 이미지의 주소입니다
+      const imageSize = new window.kakao.maps.Size(44, 50); // 마커 이미지의 크기입니다
+      const imageOption = { offset: new window.kakao.maps.Point(27, 69) }; // 마커 이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+      // 마커의 이미지 정보를 가지고 있는 마커 이미지를 생성합니다
+      const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+      const markerPosition = center; // 마커가 표시될 위치입니다 (평균 좌표)
+
+      // 마커를 생성합니다
+      this.centerMarker = new window.kakao.maps.Marker({
+        position: markerPosition,
+        image: markerImage, // 마커 이미지 설정
+      });
+
+      // 마커가 지도 위에 표시되도록 설정합니다
+      this.centerMarker.setMap(this.$refs.kakaoMap.map);
+    },
+  },
 };
 </script>
 
@@ -13,9 +95,23 @@ export default {
   <br />
   <div class="container">
     <div class="search-view">
-      <h3 style="font-family: 'neon'">좌표 클릭과 생성<br />구현 예정</h3>
+      <p style="color: black; font-size: 25px; margin-top: 10px; font-family: 'neon'">좌표를 생성해주세요</p>
+      <button @click="addMarker">좌표 추가</button>
+      <ul>
+        <li v-for="(coordinate, index) in coordinates" :key="index">
+          <hr />
+          좌표 {{ index + 1 }} &nbsp;<button @click="removeMarker(index)">삭제</button><br />
+          위도 {{ coordinate.latitude.toFixed(6) }}, 경도 {{ coordinate.longitude.toFixed(6) }}
+        </li>
+      </ul>
+      <hr />
+      <button @click="centerMapOnAverage">가운데로 모여라!</button>
+      <p style="font-family: 'neon'" v-if="averageLat && averageLng">
+        평균 위도: {{ averageLat.toFixed(6) }}<br />
+        평균 경도: {{ averageLng.toFixed(6) }}
+      </p>
     </div>
-    <KakaoMap class="kakao-map" />
+    <KakaoMap class="kakao-map" ref="kakaoMap" @updateMarkerPosition="handleMarkerPositionUpdate" />
   </div>
 </template>
 
@@ -31,5 +127,17 @@ export default {
 
 .kakao-map {
   flex: 5;
+}
+
+button {
+  font-size: 17px;
+  margin-top: 10px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  color: #00bcd4;
+  background-color: rgb(246, 246, 246);
+  cursor: pointer;
+  border-color: #05defb;
+  border-radius: 5px;
 }
 </style>
