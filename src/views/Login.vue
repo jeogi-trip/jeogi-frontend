089889dd906352e9f5affc33247a8b095bb1a316
wslim98 <dnjstjr428@naver.com>
2024-05-24 02:58:49 +0900

    Feat: 아이디 저장, 아이디/비밀번호 찾기 기능 구현

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import axiosInstance from "@/api/axiosInstance";
 import { useRouter } from "vue-router";
 import { useAuthStore } from "@/stores/auth";
@@ -9,6 +9,8 @@ const authStore = useAuthStore();
 
 const userId = ref("");
 const password = ref("");
+const saveId = ref(false);
+const showModal = ref(false);
 
 const signIn = async () => {
   try {
@@ -20,10 +22,23 @@ const signIn = async () => {
     authStore.login(userId.value);
     await router.replace("/");
     window.location.reload(); // 페이지 새로고침
+    if (saveId.value) {
+      localStorage.setItem("savedUserId", userId.value);
+    } else {
+      localStorage.removeItem("savedUserId");
+    }
   } catch (error) {
     alert("로그인 실패: " + error.message);
   }
 };
+
+onMounted(() => {
+  const savedUserId = localStorage.getItem("savedUserId");
+  if (savedUserId) {
+    userId.value = savedUserId;
+    saveId.value = true;
+  }
+});
 </script>
 
 <template>
@@ -67,8 +82,29 @@ const signIn = async () => {
                 </div>
               </div>
               <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                <input type="checkbox" class="form-check-input" id="exampleCheck1" v-model="saveId" />
                 <label class="form-check-label" for="exampleCheck1">아이디 저장</label>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <span style="color: gray; cursor: pointer" @click="showModal = true">아이디/비밀번호 찾기</span>
+                <div v-if="showModal" class="modal-mask" @click="showModal = false">
+                  <div class="modal-wrapper" style="color: black" @click.stop>
+                    <div class="modal-container">
+                      <div class="modal-header">
+                        <span>알림</span>
+                        <button style="color: gray; cursor: pointer" @click="showModal = false">x</button>
+                      </div>
+                      <div class="modal-body">
+                        <p style="color: black">
+                          고객센터에 문의하세요.<br /><br /><i class="lni-user"></i> 김태균 상담원<br /><i
+                            class="lni-phone"
+                          ></i>
+                          010-4677-9695<br /><i class="lni-user"></i> 임원석 상담원<br /><i class="lni-phone"></i>
+                          010-8301-4988
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
               <button @click.prevent="signIn" class="btn btn-common log-btn">로그인</button>
             </form>
@@ -85,4 +121,38 @@ const signIn = async () => {
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+* {
+  font-family: "neon";
+}
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-wrapper {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+.modal-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.modal-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
